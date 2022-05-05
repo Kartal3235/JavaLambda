fc2ff56a8710021821473c826ed4045c1aac80cd
Bahadır Koramaz <bkork1maz@gmail.com>
2022-05-05 20:02:29 +0300

    05.05.22

---
+++ src/day06/Lambda06.java
@@ -0,0 +1,83 @@
+package lambdaTutorial;
+
+import day01.Lambda01;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Lambda06 {
+
+    public static void main(String[] args) throws IOException {
+        //TASK 01 --> haluk.txt dosyasini okuyunuz.(Console'a yazdiriniz)
+        System.out.println("\n*** haluk.txt dosyasini okuyunuz -->  ");
+
+        Files. lines(Paths.get("src/haluk.txt")).
+                //Files classdan lines() method call edilerek
+                // data çekilecek dosya yolu (path)parametre
+                // girilerek path deki dosyanın datları akışa alındı
+
+                forEach(System.out::println);// akışdaki datalar (her satırdaki string)yazıldı.
+
+
+        //TASK 02 --> haluk.txt dosyasini buyuk harflerle okuyunuz.(Console'a buyuk harflerle yazdiriniz)
+        System.out.println("\n*** haluk.txt dosyasini buyuk harflerle okuyunuz -->  ");
+
+        Files. lines(Paths.get("src/haluk.txt")).
+
+                map(String::toUpperCase).// akışdaki datalar buyuk harfe update edildi.
+
+                        forEach(System.out::println);// akışdaki datalar (her satırdaki string)yazıldı
+
+        //TASK 03 --> haluk.txt dosyasindaki ilk satiri kucuk harflerle yazdiriniz.
+        System.out.println("\n*** haluk.txt dosyasindaki ilk satiri kucuk harflerle okuyunuz 01 -->  ");
+
+        Files. lines(Paths.get("src/haluk.txt")).
+
+                limit(1).// ilk satırı yazdır
+
+                map(String::toLowerCase).// akışdaki datalar kucuk  harfe update edildi
+
+                forEach(System.out::println);// akışdaki datalar (her satırdaki string)yazıldı
+
+
+
+        //TASK 04 --> haluk.txt dosyasinda "basari" kelimesinin kac satirda gectiginiz yazdiriniz
+        System.out.println("\n*** haluk.txt dosyasinda basari kelimesinin kac satirda gectiginiz yazdiriniz -->  ");
+
+
+
+
+
+        //TASK 05 --> haluk.txt dosyasindaki farkli kelimeleri  yazdiriniz.
+        System.out.println("\n*** haluk.txt dosyasindaki farkli kelimeleri  yazdiriniz. -->  ");
+
+
+        //TASK 06 --> haluk.txt dosyasindaki tum kelimeleri natural order  yazdiriniz.
+        System.out.println("\n*** haluk.txt dosyasindaki tum kelimeleri natural order  yazdiriniz. -->  ");
+
+
+        //TASK 07 --> haluk.txt dosyasinda "basari" kelimesinin kac kere gectigini buyuk harf kucuk harf bagımsız yaziniz.
+        System.out.println("\n*** haluk.txt dosyasinda basari kelimesinin kac kere gectigini  yazdiriniz. -->  ");
+
+
+        //TASK 08 --> haluk.txt dosyasinda "a" harfi gecen kelimelerin sayisini ekrana yazdiran programi yaziniz
+        System.out.println("\n*** haluk.txt dosyasinda a harfi gecen kelimelerin sayisini ekrana yazdiran programi yazdiriniz. -->  ");
+
+
+        //TASK 09 --> haluk.txt dosyasinda icinde "a" harfi gecen kelimeleri yazdiriniz
+        System.out.println("\n*** haluk.txt dosyasinda a harfi gecen kelimeler yazdiriniz. -->  ");
+
+
+        //TASK 10 --> haluk.txt dosyasinda kac /farklı harf kullanildigini yazdiriniz
+        System.out.println("\n*** haluk.txt dosyasinda kac /farklı harf kullanildigini  yazdiriniz. -->  ");
+
+
+        //TASK 11 --> haluk.txt dosyasinda kac farkli kelime kullanildigini yazdiriniz
+        System.out.println("\n*** haluk.txt dosyasinda kac farkli kelime kullanildigini  yazdiriniz. -->  ");
+
+
+    }
+}
